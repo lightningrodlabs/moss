a00639f0b0f94744b72e746412410afb919ae02e
Matthias <tuttiacc@hotmail.com>
2024-04-29 21:45:55 +0200

    adapted devSetup to new tools library

--- src/main/sharedTypes.ts
+++ src/main/sharedTypes.ts
@@ -1,3 +1,5 @@
+import { ActionHash } from '@holochain/client';
+
 export const TOOLS_LIBRARY_APP_ID = 'ToolsLibrary';
 
 // ATTENTION: If this type is changed, the same type in src/renderer/types needs to be changed as well.
@@ -16,3 +18,27 @@ export type AppHashes =
       type: 'happ';
       sha256: string;
     };
+
+export type Tool = {
+  developer_collective: ActionHash;
+  permission_hash: ActionHash; // Either the CreateAction hash of the DeveloperCollective entry or an ActionHash of a ContributorPermission entry
+  title: string;
+  subtitle: string;
+  description: string;
+  icon: string; // base64 string
+  version: string;
+  source: string; // JSON string containing information about where to get this Tool from
+  hashes: string; // Hashes related to this Tool to verify its integrity
+  changelog: string | undefined;
+  meta_data: string | undefined;
+  deprecation: string | undefined;
+};
+
+export type DeveloperCollective = {
+  name: string;
+  description: string;
+  website: string;
+  contact: string;
+  icon: string;
+  meta_data: string | undefined;
+};
