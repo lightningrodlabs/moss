0d93f4ad1c769202b091dd374db1f1ea691d1207
Matthias <tuttiacc@hotmail.com>
2024-06-28 10:40:40 +0200

    added export logs menu item

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,6 +1,7 @@
-import { Menu, dialog, shell } from 'electron';
+import { Menu, app, dialog, shell } from 'electron';
 import { WeFileSystem } from './filesystem';
 import { isMac } from './utils';
+import AdmZip from 'adm-zip';
 
 // extending from electron's default menu: https://github.com/electron/electron/blob/398dde9dfbdfcfd7757ead9a30785c01de9f0808/lib/browser/default-menu.ts#L12
 export const mossMenu = (mossFileSystem: WeFileSystem) => {
@@ -15,6 +16,26 @@ export const mossMenu = (mossFileSystem: WeFileSystem) => {
             await shell.openPath(mossFileSystem.appLogsDir);
           } catch (e) {
             dialog.showErrorBox('Failed to open logs folder', (e as any).toString());
+          }
+        },
+      },
+      {
+        label: 'Export Logs',
+        async click() {
+          try {
+            const zip = new AdmZip();
+            zip.addLocalFolder(mossFileSystem.appLogsDir);
+            const exportToPathResponse = await dialog.showSaveDialog({
+              title: 'Export Logs',
+              buttonLabel: 'Export',
+              defaultPath: `Moss_${app.getVersion()}_logs_${new Date().toISOString()}.zip`,
+            });
+            if (exportToPathResponse.filePath) {
+              zip.writeZip(exportToPathResponse.filePath);
+              shell.showItemInFolder(exportToPathResponse.filePath);
+            }
+          } catch (e) {
+            dialog.showErrorBox('Failed to export logs', (e as any).toString());
           }
         },
       },
