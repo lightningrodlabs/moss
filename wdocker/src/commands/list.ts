07354b3cbab896872fb2485da587baf611022676
Matthias <tuttiacc@hotmail.com>
2024-09-24 12:59:08 +0200

    sorting conductors in list command

--- wdocker/src/commands/list.ts
+++ wdocker/src/commands/list.ts
@@ -26,16 +26,29 @@ export async function list(): Promise<void> {
     style: { 'padding-left': 0, 'padding-right': 10 },
   });
   table.push(['CONDUCTOR ID', 'CREATED', 'STATUS', 'SIZE']);
-  const infos = allConductorInfos.map((info) => [
-    info.id,
-    format(info.createdAt),
-    info.status === 'running'
-      ? info.startedAt
-        ? `running (started ${format(info.startedAt)})`
-        : 'Running (Unknown duration)'
-      : 'stopped',
-    info.size ? info.size : 'unknown size',
-  ]);
+  const infos = allConductorInfos
+    .sort((info_a, info_b) => {
+      if (info_a.status === 'running' && info_b.status === 'stopped') return -1;
+      if (info_a.status === 'stopped' && info_b.status === 'running') return 1;
+      if (
+        info_a.status === 'running' &&
+        info_b.status === 'running' &&
+        info_b.startedAt &&
+        info_a.startedAt
+      )
+        return info_b.startedAt - info_a.startedAt;
+      return info_b.createdAt - info_a.createdAt;
+    })
+    .map((info) => [
+      info.id,
+      format(info.createdAt),
+      info.status === 'running'
+        ? info.startedAt
+          ? `running (started ${format(info.startedAt)})`
+          : 'Running (Unknown duration)'
+        : 'stopped',
+      info.size ? info.size : 'unknown size',
+    ]);
   infos.forEach((info) => table.push(info));
   console.log(table.toString());
 }
