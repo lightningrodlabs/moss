a7ce08669684b7c5aa5d978929126e4f565dab42
Matthias <tuttiacc@hotmail.com>
2024-10-16 22:19:28 +0200

    fix iframe timing bug

--- iframes/applet-iframe/src/index.ts
+++ iframes/applet-iframe/src/index.ts
@@ -222,6 +222,15 @@ const weaveApi: WeaveServices = {
   if (view.type === 'applet-view') {
     if (iframeConfig.type !== 'applet') throw new Error('Bad iframe config');
 
+    const [profilesClient, appletClient] = await Promise.all([
+      setupProfilesClient(
+        iframeConfig.appPort,
+        iframeConfig.profilesLocation.authenticationToken,
+        iframeConfig.profilesLocation.profilesRoleName,
+      ),
+      setupAppletClient(iframeConfig.appPort, iframeConfig.authenticationToken),
+    ]);
+
     const appletHash = window.__WEAVE_APPLET_HASH__;
 
     // message handler for ParentToApplet messages - Only added for applet main-view
@@ -248,15 +257,6 @@ const weaveApi: WeaveServices = {
         });
       },
     );
-
-    const [profilesClient, appletClient] = await Promise.all([
-      setupProfilesClient(
-        iframeConfig.appPort,
-        iframeConfig.profilesLocation.authenticationToken,
-        iframeConfig.profilesLocation.profilesRoleName,
-      ),
-      setupAppletClient(iframeConfig.appPort, iframeConfig.authenticationToken),
-    ]);
 
     window.__WEAVE_RENDER_INFO__ = {
       type: 'applet-view',
