e0d9df8938f760511dc46266528c0440eb71914a
Matthias <tuttiacc@hotmail.com>
2024-12-04 21:13:30 +0100

    fix possible case of creating main window if app is not ready

--- src/main/index.ts
+++ src/main/index.ts
@@ -1648,8 +1648,8 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    MAIN_WINDOW = createOrShowMainWindow();
+  if (MAIN_WINDOW) {
+    createOrShowMainWindow();
   }
 });
 
