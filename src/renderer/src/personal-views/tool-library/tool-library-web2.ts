f04a6073c9bef993174199b27b0e14d8f293725b
Matthias <tuttiacc@hotmail.com>
2024-12-19 20:57:11 +0100

    add production curation list

--- src/renderer/src/personal-views/tool-library/tool-library-web2.ts
+++ src/renderer/src/personal-views/tool-library/tool-library-web2.ts
@@ -36,7 +36,7 @@ import { deriveToolCompatibilityId } from '@theweave/utils';
 
 const PRODUCTION_TOOL_CURATION_CONFIGS: ToolCurationConfig[] = [
   {
-    url: 'https://raw.githubusercontent.com/lightningrodlabs/weave-tool-curation/refs/heads/test-0.13/0.13/lists/curations-0.13.json',
+    url: 'https://lightningrodlabs.org/weave-tool-curation/0.13/curations-0.13.json',
     useLists: ['default'],
   },
 ];
