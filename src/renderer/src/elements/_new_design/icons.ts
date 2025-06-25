7c3a564fc419043ef40d2d8b4367ef5bae6da77f
Matthias <tuttiacc@hotmail.com>
2025-06-25 20:29:13 +0200

    add experimental badges and fix sorting logic

--- src/renderer/src/elements/_new_design/icons.ts
+++ src/renderer/src/elements/_new_design/icons.ts
@@ -310,3 +310,33 @@ export const sendIcon = (size = 16) => html`
     />
   </svg>
 `;
+
+export const flaskIcon = (size = 16) => html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width=${size}
+    height=${size}
+    fill="currentColor"
+    class="bi bi-flask"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M4.5 0a.5.5 0 0 0 0 1H5v5.36L.503 13.717A1.5 1.5 0 0 0 1.783 16h12.434a1.5 1.5 0 0 0 1.28-2.282L11 6.359V1h.5a.5.5 0 0 0 0-1zM10 2H9a.5.5 0 0 0 0 1h1v1H9a.5.5 0 0 0 0 1h1v1H9a.5.5 0 0 0 0 1h1.22l.61 1H10a.5.5 0 1 0 0 1h1.442l.611 1H11a.5.5 0 1 0 0 1h1.664l.611 1H12a.5.5 0 1 0 0 1h1.886l.758 1.24a.5.5 0 0 1-.427.76H1.783a.5.5 0 0 1-.427-.76l4.57-7.48A.5.5 0 0 0 6 6.5V1h4z"
+    />
+  </svg>
+`;
+
+export const flaskIconRound = (size = 16) => html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width=${size}
+    height=${size}
+    fill="currentColor"
+    class="bi bi-flask-florence"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M5.5 0a.5.5 0 0 0 0 1H6v5.416a5 5 0 1 0 4 0V1h.5a.5.5 0 0 0 0-1zM9 2h-.5a.5.5 0 0 0 0 1H9v1h-.5a.5.5 0 0 0 0 1H9v1h-.5a.5.5 0 0 0 0 1h.564a.5.5 0 0 0 .27.227A4.002 4.002 0 0 1 8 15a4 4 0 0 1-1.333-7.773.5.5 0 0 0 .333-.47V1h2z"
+    />
+  </svg>
+`;
