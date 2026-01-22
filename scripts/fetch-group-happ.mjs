c62222924620ba4f167b9f81712eaab1f4e9a89b
ddd-mtl <doute.d@gmail.com>
2026-01-22 15:07:47 -0500

    ensure target directory exists before downloading group.happ

--- scripts/fetch-group-happ.mjs
+++ scripts/fetch-group-happ.mjs
@@ -5,8 +5,11 @@ import {downloadFile} from "./fetch-fns.mjs";
 const mossConfigJSON = fs.readFileSync('moss.config.json');
 const mossConfig = JSON.parse(mossConfigJSON);
 
+const targetDir = path.join('resources', 'default-apps');
+fs.mkdirSync(targetDir, {recursive: true});
+
 downloadFile(
   `https://github.com/lightningrodlabs/moss/releases/download/group-happ-v${mossConfig.groupHapp.version}/group.happ`,
-  path.join('resources', 'default-apps', 'group.happ'),
+  path.join(targetDir, 'group.happ'),
   mossConfig.groupHapp.sha256,
 );
